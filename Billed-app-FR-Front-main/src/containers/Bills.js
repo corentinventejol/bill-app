20c8688dd703df40168152b2450dd45440750c96
corentinventejol <corentinventejol78@gmail.com>
2025-01-14 14:21:31 +0100

    wip bills / test

--- Billed-app-FR-Front-main/src/containers/Bills.js
+++ Billed-app-FR-Front-main/src/containers/Bills.js
@@ -30,31 +30,37 @@ export default class {
   getBills = () => {
     if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status)
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot
+            .map(doc => ({
+              ...doc,
+              date: doc.date,
+              status: formatStatus(doc.status)
+            }))
+            .sort((a, b) => new Date(b.date) - new Date(a.date)) // Tri décroissant par date
+            .map(doc => {
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status)
+                }
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, 'for', doc)
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status)
+                }
               }
-            } catch(e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
-            }
-          })
+            })
           console.log('length', bills.length)
-        return bills
-      })
+          return bills
+        })
     }
   }
 }
